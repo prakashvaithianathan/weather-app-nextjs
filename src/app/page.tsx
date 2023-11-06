62580a27a831a6a90b608ca18efc353e54be3fa7
prakashsparkz <indupraka18@gmail.com>
2023-11-07 01:15:00 +0530

    responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 "use client"
 
 import { useEffect, useState, ChangeEvent } from 'react'
+
 import Image from 'next/image'
 import { getWeatherBySearch } from '@/api/current_weather'
 import moment from "moment-timezone"
 import Input from '@/components/Input'
+import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 
 
 interface WeatherData {
@@ -57,9 +60,20 @@ interface WeatherData {
             icon: string;
             code: number;
           },
-          avgtemp_c: string
+          avgtemp_c: string;
+          avghumidity: number;
+          daily_chance_of_rain: number;
+          daily_chance_of_snow: number;
+          maxtemp_c: number;
+          maxwind_kph: number;
+          uv: number
         },
-        astro: object,
+        astro: {
+          moonrise: string,
+          moonset: string,
+          sunrise: string,
+          sunset: string
+        },
         hour: [
           {
             condition: {
@@ -84,8 +98,20 @@ interface timeData {
   dayAndDate: string
 }
 
+interface dynamicWeatherData {
+  temp: number;
+  feels_temp: number;
+  text: string;
+  humidity: number;
+  pressure: number;
+  wind: number;
+  uv: number;
+  is_day: number
+}
+
 export default function Home() {
 
+  const router = useRouter()
   const [weatherData, setWeatherData] = useState<WeatherData>({
     location: {
       name: '',
@@ -136,9 +162,20 @@ export default function Home() {
               icon: '',
               code: 0,
             },
-            avgtemp_c: ""
+            avgtemp_c: "",
+            avghumidity: 0,
+            daily_chance_of_rain: 0,
+            daily_chance_of_snow: 0,
+            maxtemp_c: 0,
+            maxwind_kph: 0,
+            uv: 0
           },
-          astro: {},
+          astro: {
+            moonrise: "",
+            moonset: "",
+            sunrise: "",
+            sunset: ""
+          },
           hour: [
             {
               condition: {
@@ -160,6 +197,17 @@ export default function Home() {
     time: "",
     meridiem: "",
     dayAndDate: ""
+  })
+
+  const [dynamicWeatherData, setDynamicWeatherData] = useState<dynamicWeatherData>({
+    temp: 0,
+    feels_temp: 0,
+    text: "",
+    humidity: 0,
+    pressure: 0,
+    wind: 0,
+    uv: 0,
+    is_day: 0
   })
 
 
@@ -192,11 +240,20 @@ export default function Home() {
         getWeather(getWeatherData.location.tz_id)
       }, 1000);
       setWeatherData(getWeatherData)
+      setDynamicWeatherData({
+        temp: 0,
+        feels_temp: 0,
+        text: "",
+        humidity: 0,
+        pressure: 0,
+        wind: 0,
+        uv: 0,
+        is_day: 0
+      })
     } catch (error) {
 
     }
   }
-
 
 
   const getDayfromDate = (date: string) => {
@@ -231,12 +288,20 @@ export default function Home() {
   }
 
 
+  // const goToDetailsPage = (item: WeatherData) => {
+
+  //   router.push('/details', {
+  //     search: { data: JSON.stringify(item) }
+  //   });
+  // }
+
+
 
   return (
     <main >
       <div>
         <div className='flex justify-center items-center mt-10'>
-          <div className=' backdrop-blur-xl flex  items-center gap-5 bg-dull_grey/50 w-1/2 p-2 rounded-full shadow-white'>
+          <div className=' backdrop-blur-xl flex  items-center gap-5 bg-dull_grey/50 w-max lg:w-1/2 p-2 rounded-full shadow-white'>
             <img src="/icons/search.png" alt="search" className='w-4 ml-3' />
             <Input
               placeholder='Search city, country'
@@ -250,53 +315,53 @@ export default function Home() {
             />
           </div>
         </div>
-        <div className='w-11/12 flex items-center justify-center gap-5 mt-16 m-auto'>
-          <div className='backdrop-blur-xl bg-dull_grey/50 w-1/4 h-96 rounded-3xl'>
-            <h2 className='text-center text-gold md:text-4xl 2xl:text-5xl font-medium mt-10'>{weatherData.location.name}</h2>
-            <div className='mt-16'>
+        <div className='w-11/12 flex flex-col lg:flex-row  items-center justify-center gap-5 mt-16 m-auto'>
+          <div className='backdrop-blur-xl bg-dull_grey/50 w-full lg:w-1/4  h-64 lg:h-96 rounded-3xl'>
+            <h2 className='text-center text-gold text-4xl 2xl:text-5xl font-medium mt-10'>{weatherData.location.name}</h2>
+            <div className='mt-8 lg:mt-16'>
               <div className='flex items-end justify-center'>
-                <h2 className='md:text-6xl 2xl:text-8xl font-medium'>{timeData.time}</h2>
+                <h2 className='text-6xl 2xl:text-8xl font-medium'>{timeData.time}</h2>
                 <p className='text-2xl mb-1'>{timeData.meridiem}</p>
               </div>
               <p className='text-center text-neutral-400 md:text-lg 2xl:text-2xl'>{timeData.dayAndDate}</p>
             </div>
           </div>
-          <div className='backdrop-blur-xl bg-dull_grey/50 w-3/4 h-96 rounded-3xl flex justify-around items-center p-8'>
+          <div className='backdrop-blur-xl bg-dull_grey/50 w-full lg:w-3/4 h-max lg:h-96 rounded-3xl flex  flex-col lg:flex-row justify-around items-center p-8'>
             <div>
-              <h2 className='2xl:text-8xl md:text-xl font-bold'>{weatherData.current.temp_c}°C</h2>
+              <h2 className='text-6xl xl:text-6xl 2xl:text-8xl  font-bold'>{weatherData.current.temp_c}°C</h2>
               <div className=' text-neutral-400 flex items-center justify-center'>
-                <p className='text-2xl font-medium'>Feels like: </p>
-                <h2 className='text-5xl font-medium'>&nbsp;{weatherData.current.feelslike_c}°C</h2>
+                <p className='text-xl lg:text-2xl font-medium'>Feels like: </p>
+                <h2 className='text-2xl lg:text-5xl font-medium'>&nbsp;{weatherData.current.feelslike_c}°C</h2>
               </div>
             </div>
             <div className='w-max'>
-              <img src={`/weather_icons/${weatherData.current.is_day ? "day" : "night"}/${weatherData.current.condition.code}.png`} alt="sunny" className='w-1/2 m-auto' />
-              <p className='text-center mt-6 font-medium text-5xl'>{weatherData.current.condition.text}</p>
+              <img src={`/weather_icons/${weatherData.current.is_day ? "day" : "night"}/${weatherData.current.condition.code}.png`} alt="sunny" className='w-1/4 lg:w-1/2 m-auto mt-10 lg:mt-0' />
+              <p className='text-center mt-2  lg:mt-6 font-medium text-3xl lg:text-5xl'>{weatherData.current.condition.text}</p>
             </div>
-            <div className='grid grid-cols-2 items-center gap-12 gap-x-20'>
+            <div className='grid grid-cols-2 items-center gap-12 gap-x-20 mt-8 lg:mt-0'>
               <div className='flex flex-col items-center gap-2'>
-                <img src="/weather_icons/humidity.png" alt="humidity" />
+                <img src="/weather_icons/humidity.png" alt="humidity" className='w-1/2' />
                 <div>
                   <p className='text-2xl font-medium text-center'>{weatherData.current.humidity}%</p>
                   <p className='text-lg font-normal'>Humidity</p>
                 </div>
               </div>
               <div className='flex flex-col items-center gap-2'>
-                <img src="/weather_icons/wind.png" alt="wind" />
+                <img src="/weather_icons/wind.png" alt="wind" className='w-1/2 lg:w-14' />
                 <div>
                   <p className='text-2xl font-medium text-center'>{weatherData.current.wind_kph}km/h</p>
                   <p className='text-lg font-normal text-center'>Wind</p>
                 </div>
               </div>
               <div className='flex flex-col items-center gap-2'>
-                <img src="/weather_icons/pressure.png" alt="pressure" />
+                <img src="/weather_icons/pressure.png" alt="pressure" className='w-1/2' />
                 <div>
                   <p className='text-2xl font-medium text-center'>{weatherData.current.pressure_in}hPa</p>
                   <p className='text-lg font-normal text-center'>Pressure</p>
                 </div>
               </div>
               <div className='flex flex-col items-center gap-2'>
-                <img src="/weather_icons/uv.png" alt="uv" />
+                <img src="/weather_icons/uv.png" alt="uv" className='w-1/2' />
                 <div>
                   <p className='text-2xl font-medium text-center'>{weatherData.current.uv}</p>
                   <p className='text-lg font-normal text-center'>UV</p>
@@ -305,31 +370,64 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className='w-11/12 flex items-center justify-center gap-5 mt-8 m-auto mb-16 relative'>
-          <div className='backdrop-blur-xl w-3/4 h-96 bg-dull_grey/50 m-auto rounded-3xl relative'>
+        <div className='w-11/12 flex flex-col lg:flex-row items-center justify-center gap-5 mt-8 m-auto mb-16 relative'>
+          <div className='backdrop-blur-xl w-full lg:w-3/4 h-96 bg-dull_grey/50 m-auto rounded-3xl relative'>
             <h2 className='text-center p-2 text-3xl font-medium text-neutral-400'>Day forecast</h2>
             <div className='flex items-center gap-5 mt-8 overflow-x-scroll px-8 horizontalScroll p-2'>
               <div className='flex items-center gap-5'>
                 {weatherData.forecast.forecastday.map((item, i) => (
-                  <div
-                    key={i}
-                    className='cursor-pointer flex flex-col items-center rounded-2xl bg-neutral-700/50 w-60 h-60 p-2 border-2 border-neutral-700'
+                  <Link
+                    href={{
+                      pathname: '/details',
+                      query: {
+                        search: JSON.stringify({
+                          moonrise: item.astro.moonrise,
+                          moonset: item.astro.moonset,
+                          sunrise: item.astro.sunrise,
+                          sunset: item.astro.sunset,
+                          date: item.date,
+                          day: {
+                            avg_humidity: item.day.avghumidity,
+                            avg_temp: item.day.avgtemp_c,
+                            condition: item.day.condition,
+                            rainChance: item.day.daily_chance_of_rain,
+                            snowChance: item.day.daily_chance_of_snow,
+                            max_temp: item.day.maxtemp_c,
+                            max_wind_kph: item.day.maxwind_kph,
+                            uv: item.day.uv
+                          },
+                          location: {
+                            place: weatherData.location.name,
+                            timeData: timeData.dayAndDate
+                          }
+                        })
+                      }
+                    }}
                   >
-                    <p className='font-bold text-3xl'>{item.day.avgtemp_c}°C</p>
-                    <img
-                      src={`/weather_icons/day/${item.day.condition.code}.png`}
-                      alt={item.day.condition.text}
-                      className='w-24 mt-5'
-                    />
-                    <p className='mt-2 font-normal text-2xl'>{item.day.condition.text}</p>
-                    <p className='text-neutral-400 font-medium'>{getDayfromDate(item.date)}</p>
-                  </div>
+                    <div
+                      // onClick={() => {
+                      //   console.log(item);
+
+                      // }}
+                      key={i}
+                      className='cursor-pointer flex flex-col items-center rounded-2xl bg-neutral-700/50 w-60 h-60 p-2 border-2 border-neutral-700'
+                    >
+                      <p className='font-bold text-3xl'>{item.day.avgtemp_c}°C</p>
+                      <img
+                        src={`/weather_icons/day/${item.day.condition.code}.png`}
+                        alt={item.day.condition.text}
+                        className='w-24 mt-5'
+                      />
+                      <p className='mt-2 font-normal text-2xl'>{item.day.condition.text}</p>
+                      <p className='text-neutral-400 font-medium'>{getDayfromDate(item.date)}</p>
+                    </div>
+                  </Link>
                 ))}
               </div>
             </div>
           </div>
 
-          <div className='backdrop-blur-xl bg-dull_grey/50 w-1/4 h-96 rounded-3xl flex flex-col items-center '>
+          <div className='backdrop-blur-xl bg-dull_grey/50 w-full lg:w-1/4 h-96 p-4 lg:p-2  rounded-3xl flex flex-col items-center '>
             <h2 className='text-center pt-4 text-3xl font-medium text-neutral-400 '>Hourly forecast</h2>
             <div className='overflow-y-scroll hideScrollHourly'>
               {
@@ -345,8 +443,6 @@ export default function Home() {
                   </div>
                 ))
               }
-
-
             </div>
           </div>
         </div>
